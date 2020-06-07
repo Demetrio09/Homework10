4f8d1ceb520daecc6d2024d90cb4a652b0d63f21
Demetrio <demetrioalmeida@gmail.com>
2020-06-06 22:16:07 -0400

    Homework

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, role, email, id, officeNumber, github, school) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.id = id;
+        this.officeNumber = officeNumber;
+        this.github = github;
+        this.school = school;
+    }
+}
+
+module.exports = Employee;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee.js");
+
+class Manager extends Employee {
+    constructor(name, role, email, id, officeNumber) {
+        if (!name) {
+            throw new Error("You are missing the name.");
+        }
+        if(!role) {
+            throw new Error("You are missing the role.");
+        }
+        if(!email) {
+            throw new Error("You are missing the email.");
+        }
+        if(!id) {
+            throw new Error("You are missing the id.");
+        }
+        if(!officeNumber) {
+            throw new Error("You are missing the office number.");
+        }
+        super(name, role, email, id, officeNumber);
+    }
+}
+
+module.exports = Manager;
